f38535ca9d5fb47ebb890cfddba3526297bf93ae
Nandeesh Kamath <58938846+nandeeshkamath@users.noreply.github.com>
2022-04-05 21:00:14 +0530

    Add wake up trigger and wait for github action

--- .github/workflows/spy-scripts/undercover-bookie.sh
+++ .github/workflows/spy-scripts/undercover-bookie.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Wake up app
+curl --location --request GET "$DOMAIN_URL/actuator/health" \
+--header "Authorization: Basic $SECRET_TOKEN" \
+--header 'Content-Type: application/json'
+
+# Wait for boot up
+sleep 15
+
+# Check for ticket availability
 curl --location --request GET "$DOMAIN_URL/spy?eventId=ET00094579&regionCode=KUNA&regionSlug=kundapura&keyword=Book%20tickets" \
 --header "Authorization: Basic $SECRET_TOKEN" \
 --header 'Content-Type: application/json'
