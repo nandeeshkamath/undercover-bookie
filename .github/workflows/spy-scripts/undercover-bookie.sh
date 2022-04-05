d8123103faa07db9398034bdbf28ed04150101fb
nandeeshkamath <nandeeshkamath99@gmail.com>
2022-04-05 19:52:49 +0530

    Minor fixes to github action

--- .github/workflows/spy-scripts/undercover-bookie.sh
+++ .github/workflows/spy-scripts/undercover-bookie.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl --location --request POST "$DOMAIN_URL/spy?eventId=ET00094579&regionCode=KUNA&regionSlug=kundapura&keyword=Book%20tickets" \
+curl --location --request GET "$DOMAIN_URL/spy?eventId=ET00094579&regionCode=KUNA&regionSlug=kundapura&keyword=Book%20tickets" \
 --header "Authorization: Basic $SECRET_TOKEN" \
 --header 'Content-Type: application/json'
